a2a499258a1c08b54478e1c2a458f3bb191c0e0f
MernStack123 <muhammedsheheer99@gmail.com>
2025-07-23 01:57:34 +0530

    side bar upddated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Icons } from './Icon';
+import { Mail, MessageSquare, Smartphone } from 'lucide-react';
 
 const Footer = () => {
   return (
@@ -26,6 +27,12 @@ const Footer = () => {
             Photos
           </Link>
           <Link
+            href={'/about'}
+            className={`font-oswald text-sm font-[700] uppercase tracking-[2px] text-[#fff] hover:underline`}
+          >
+            About
+          </Link>
+          <Link
             href={'/videos'}
             className={`font-oswald text-sm font-[700] uppercase tracking-[2px] text-[#fff] hover:underline`}
           >
@@ -39,19 +46,27 @@ const Footer = () => {
           </Link>
         </div>
         <div className="flex flex-row items-center justify-center gap-4 md:gap-6">
-          <Link href={'#'} target="_blank">
+          <Link
+            href={
+              'https://www.instagram.com/vintagian_?igsh=b3JwbGkxY2c0dTZt&utm_source=qr'
+            }
+            target="_blank"
+          >
             <Icons.instagram className="text-white" />
           </Link>
-          <Link href={'#'} target="_blank">
-            <Icons.facebook className="text-white" />
+          <Link href="https://wa.me/917034732231?text=Hi%20Jamsheer,%20I'm%20interested%20in%20your%20photography%20services.">
+            <MessageSquare size={28} className="text-white" />
           </Link>
-          <Link href={'#'} target="_blank">
-            <Icons.unknown className="text-white" />
+          <Link href={'tel:+917034732231'}>
+            <Smartphone size={28} className="text-white" />
+          </Link>
+          <Link href={'mailto:jamsheermaliyakkal@gmail.com'}>
+            <Mail size={28} className="text-white" />
           </Link>
         </div>
         <div>
           <span className="font-montserrat text-xs font-[300] text-white">
-            © 2025 — A Jamsheer Class Photography Production
+            © 2025 — Moments Captured, Stories Told — Jamsheer
           </span>
         </div>
       </div>
